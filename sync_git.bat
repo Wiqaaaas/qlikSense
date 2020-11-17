5aead620097d50094f7f94378872bd4216274198
Waqas Ahmed <72467937+Wiqaaaas@users.noreply.github.com>
2020-11-17 11:53:43 +0500

    Update sync_git.bat

--- sync_git.bat
+++ sync_git.bat
@@ -2,5 +2,5 @@ rem C:\data_git\qliksense_mods
 cd C:\data_git\qliksense_mods
 git pull
 rem UnComment out the next two lines to auto restart Proxy
-stop "Qlik Sense Proxy Service"
-start "Qlik Sense Proxy Service"
+net stop "Qlik Sense Proxy Service"
+net start "Qlik Sense Proxy Service"
